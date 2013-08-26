f6031da3605a237e5b9af6e8ab51da8309f5d978
Abi Noda <a@abinoda.com>
2013-08-26 10:56:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nedjer::Application.config.secret_token = 'dda63f8f89fdd2b3f5e5584a0a05e7b6e215a38456f4f49b7d3458526d76c7c013a2ef33fabf731124c18977a1fcc7ced910e4273c6aee9de2beea3c66513239'
