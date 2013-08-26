4e9bbecc37fa71a0dfe2a824408f102fd13a91e8
Abi Noda <a@abinoda.com>
2013-08-26 18:18:14 -0500

    Added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Nedjer::Application.routes.draw do
   # get "/users/:id", to: 'users#show', as: 'show_user'
-  get '/login', :to => 'sessions#new'
+  # get '/login', :to => 'sessions#new'
   post '/login', :to => 'sessions#create', as: 'login'
   delete '/logout', :to => 'sessions#destroy', :via => :delete
 
